c40d9af8a045d4500076668900cbdfc133f012d1
Henry Hutcheson <henryhutcheson9@gmail.com>
2026-02-05 16:03:56 -0500

    Fix Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/profile-app/",
+
+  // GitHub Pages repo name
+  base: "/lab7/",
+
+  // Output folder GitHub Pages can serve
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
